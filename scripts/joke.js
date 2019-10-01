52f7ae3c8b36e2b4d78630108a40a9b7e8766298
dek-haji <deeeqhaji@gmail.com>
2019-10-01 11:54:39 -0500

    added new refresh button

--- scripts/joke.js
+++ scripts/joke.js
@@ -7,35 +7,31 @@ let setup = document.querySelector(".jokes");
 let punchline = document.querySelector(".answers");
 let btn = document.querySelector(".btn")
 
+function refreshPage() {
+    window.location.reload();
+}
 let jokeMaker = (jokeObj) => {
     setup.innerHTML = `<h5> ${jokeObj.setup}</h5>`
-   
 }
 
 fetch("https://official-joke-api.appspot.com/random_joke")
     .then(jokes => jokes.json())
     .then(parsedJokes => {
-        
             console.log(parsedJokes)
             // 2. Console log the setup and punchline
             console.log(parsedJokes.setup)
             // 3. Display the joke on the DOM and punchline on the DOM
             console.log(parsedJokes.punchline)
-            
         jokeMaker(parsedJokes)
 
-            btn.addEventListener("click", function(){
-            punchline.innerHTML = parsedJokes.punchline
+        btn.addEventListener("click", function () {
+                punchline.innerHTML = parsedJokes.punchline
+                console.log("is working")
             })
         })
-        
-
-
-
-        
 // Bonus 1
 // 4. Display only the setup and then...
-// 5. Incorporate a button with a `click` listener that 
+// 5. Incorporate a button with a `click` listener that
 // when triggered will display the punchline.
 
 // Bonus 2
